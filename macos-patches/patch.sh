4a84aa35b2215cd4befb51e19070c58a6da8b2ef
Touchstone64 <touchstone64@gweb.me.uk>
2026-04-21 17:41:45 +0100

    Remove the last of the reporting code

--- macos-patches/patch.sh
+++ macos-patches/patch.sh
@@ -5,23 +5,20 @@ SCRIPT_NAME=$(basename ${0})
 MACOS_DIR="$1"
 PATCH="$2"
 
-if [[ ! -n ${MACOS_DIR} ]] || [[ ! -n ${PATH} ]]; then
+if [[ ! -n ${MACOS_DIR} ]] || [[ ! -n ${PATCH} ]]; then
     echo "usage: ${SCRIPT_NAME} <path to packaging/macos> <name of patch file>"
     exit 1
 fi
-
-set -e 
 
 if [[ ! -f ${PATCH} ]]; then
     echo "${SCRIPT_NAME}: patch '${PATCH}' not found"
     exit 2
 fi
 
+set -e 
+
 TARGET=$(grep "+++" ${PATCH} | cut -f 1 | cut -d ' ' -f 2)
 TARGET_PATH=${MACOS_DIR}/${TARGET}
-
-echo "TARGET: ${TARGET}"
-echo "TARGET_PATH: ${TARGET_PATH}"
 
 if [[ ! -f ${TARGET_PATH} ]]; then
     echo "${SCRIPT_NAME}: target '${TARGET_PATH}' not found"
